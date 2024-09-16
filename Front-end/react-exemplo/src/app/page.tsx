1ee320b948d3466a6e7ede73f44394c775b7e8ed
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-09-16 20:23:40 -0300

    feat: update aplications

--- Front-end/react-exemplo/src/app/page.tsx
+++ Front-end/react-exemplo/src/app/page.tsx
@@ -1,19 +1,14 @@
-import Image from "next/image";
+import Image from 'next/image';
+import {Header} from '@/components/header';
+import {Grid} from '@/components/grid';
 
 export default function Home() {
   return (
     <>
-    <div className="h-screen">
-      <div className="bg-gray-800  text-white h-1/2">
-        <h2 className="text-white h-5 text-2xl text-center">Lemonade</h2>
-        <p className="m-6 text-center text-4xl align-middle">
-         Stories about Life, <br />
-         Tech & Insurance
-        </p>
-        <p className="m-5 text-sm text-center">Subscribe to our exclusive page</p>
+      <div className="h-screen">
+        <Header />
+        <Grid />
       </div>
-    </div>
-
     </>
   );
 }
