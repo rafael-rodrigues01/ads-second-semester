747dbf3898b8d1668513e178e89be7c603a92663
rafael-rodrigues01 <rafa_rodrigues01@yahoo.com>
2024-10-22 20:45:51 -0300

    feat: update front

--- Front-end/iot/src/app/users/page.tsx
+++ Front-end/iot/src/app/users/page.tsx
@@ -1,17 +1,15 @@
-import Sidebar from '@/components/layouts/Sidebar';
-import Header from '@/components/layouts/Header';
+
 import React from 'react';
-import PhotoCard from '@/components/cards/PhotoCard'
+import Profile from './profile/page';
 
 
 export default function User() {
   return (
     <div className="flex">
-      <Sidebar />
       <div className="flex-1 flex flex-col">
-        <Header title="Usuários" username="Rafael Rodrigues" />
         <main className="flex-1">
-          <PhotoCard />
+          {/* <PhotoCard /> */}
+          <Profile />
         </main>
       </div>
     </div>
